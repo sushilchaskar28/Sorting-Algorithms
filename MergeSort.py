e8f58ea1e723b9bf7a7aa85fe720c8e882a36056
sushilchaskar28 <sushilchaskar28@gmail.com>
2021-10-05 20:33:17 -0500

    Update MergeSort.py

--- MergeSort.py
+++ MergeSort.py
@@ -12,29 +12,29 @@ For more: https://en.wikipedia.org/wiki/Merge_sort
 
 def mergeSort(arr):
     if len(arr)>1:
-    m=len(arr)//2
-    l=mergeSort(arr[:m]) #Divide and merge to get left sorted array
-    r=mergeSort(arr[m:]) # Divide and merge to get right sorted array
-    
-    # merge two subarrays 
-    i,j,k=0,0,0
-    while i<len(l) and j<len(r):
-        if l[i]<=r[j]:
-        arr[k]=l[i]
-        i+=1
-        else:
-        arr[k]=r[j]
-        j+=1
-        k+=1
-    while i<len(l):
-        arr[k]=l[i]
-        i+=1
-        k+=1
-    while j<len(r):
-        arr[k]=r[j]
-        j+=1
-        k+=1
+        m=len(arr)//2
+        l=mergeSort(arr[:m]) #Divide and merge to get left sorted array
+        r=mergeSort(arr[m:]) # Divide and merge to get right sorted array
+
+        # merge two subarrays 
+        i,j,k=0,0,0
+        while i<len(l) and j<len(r):
+            if l[i]<=r[j]:
+                arr[k]=l[i]
+                i+=1
+            else:
+                arr[k]=r[j]
+                j+=1
+            k+=1
+        while i<len(l):
+                arr[k]=l[i]
+                i+=1
+                k+=1
+        while j<len(r):
+                arr[k]=r[j]
+                j+=1
+                k+=1
     return arr
     
 #Let's check with example:
-print(mergeSort([7,4,8,6,2,15,5])
+print(mergeSort([7,4,8,6,2,15,5]))
